4de4fc2213014f7a81cab998bd499e255f7f107c
nitya <nityanandabehera85132@gmail.com>
2022-02-04 16:57:56 +0530

    first commit

---
+++ nitya/base/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.test import tag
+from.models import Project,Review,Tag
+admin.site.register(Project)
+admin.site.register(Review)
+admin.site.register(Tag)
+
+# Register your models here.
