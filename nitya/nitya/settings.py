e46d5c3bc75ef879018d4e39527e2c49fadbd7c6
nitya <nityanandabehera85132@gmail.com>
2022-02-09 10:29:51 +0530

    theme installation

--- nitya/nitya/settings.py
+++ nitya/nitya/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--x8o&=z&wkc$2g32yf)xvn+xn&vg6857^0w%_mskb064+xhe$-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
